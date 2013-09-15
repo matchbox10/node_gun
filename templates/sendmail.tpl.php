d53bb54fc648f45e543abf6e32e27be42af9018e
Mark Simpson <mark.simpson78@hotmail.co.uk>
2013-09-15 18:03:27 +0100

    Initial Commit

---
+++ templates/sendmail.tpl.php
@@ -0,0 +1,33 @@
+<?php
+
+mailgun_init('key-757-cudz0w3c807fkvys81tlwsmnm1x3');
+
+// Plain text message
+/*MailgunMessage::send_text("user@matchbox10.mailgun.org", 
+                          "'John Doe' <mark.simpson78@hotmail.co.uk>",
+                          "Hello text PHP API!",
+                          "Hi!\nI am sending a message using Mailgun PHP API");*/
+
+// Plain text message + tag
+/*MailgunMessage::send_text("user@matchbox10.mailgun.org", 
+                          "'John Doe' <mark.simpson78@hotmail.co.uk>",
+                          "Hello text PHP API + tag!",
+                          "Hi!\nI am sending a message using Mailgun PHP API",
+                          "",
+                          array("headers" => array(MAILGUN_TAG => "sample_text_php")));*/
+
+// MIME message
+$rawMime = 
+    "X-Priority: 1 (Highest)\n".
+    "X-Mailgun-Tag: sample_raw_php\n".
+    "Content-Type: text/plain;charset=UTF-8\n".    
+    "From: user@matchbox10.mailgun.org\n".
+    "To: $emailName".
+    "Subject: Hello raw PHP API!\n".
+    "\n".
+    "This message is sent by Mailgun PHP API";
+MailgunMessage::send_raw($emailName, "mark.simpson78@hotmail.co.uk", $rawMime); 
+
+echo "Messages sent\n";
+
+?>
